4de0bfe2c9285db5cf3cd8964d313fbc8d9dfcda
HYE MIN PARK <113421365+phmhp@users.noreply.github.com>
2025-06-06 14:11:56 +0900

    [level 0] Title: 배열 만들기 2, Time: 87.83 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int l, int r) {
+    vector<int> answer;
+    for (int i = l ; i <= r; i++){
+        string s = to_string(i);
+        bool valid = true;
+        for (char c : s) {
+            if (c!='0' && c!='5') {
+                valid = false;
+                break;
+            }
+        }
+        if (valid) answer.push_back(i);
+    }
+    if (answer.empty()) answer.push_back(-1);
+    else {
+        sort(answer.begin(), answer.end());
+    }
+    return answer;
+}
