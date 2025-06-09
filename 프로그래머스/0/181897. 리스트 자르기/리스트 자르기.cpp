11ac31d4ca8c62c378da20a48729c76a5e6a563d
HYE MIN PARK <113421365+phmhp@users.noreply.github.com>
2025-06-09 19:07:13 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, vector<int> slicer, vector<int> num_list) {
+    vector<int> answer;
+    switch 
+    return answer;
+}
