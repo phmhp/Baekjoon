0716d281066fcf4cc137ea92abc4d0cc27f0cfda
HYE MIN PARK <113421365+phmhp@users.noreply.github.com>
2025-06-13 14:39:49 +0900

    [level 0] Title: 세 개의 구분자, Time: 42.27 ms, Memory: 28.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string myStr) {
+    vector<string> answer;
+    string temp= "";
+    for (int i = 0 ; i< myStr.size();i++){
+        if (myStr[i] != 'a' && myStr[i] != 'b' && myStr[i] != 'c') {
+            temp += myStr[i];
+        }
+        else if (temp != ""){
+                answer.push_back(temp);
+            temp = "";}
+        
+    }
+    if (temp != "") answer.push_back(temp);
+        
+    
+    
+    if (answer.empty()) answer.push_back("EMPTY");
+    return answer;
+}
