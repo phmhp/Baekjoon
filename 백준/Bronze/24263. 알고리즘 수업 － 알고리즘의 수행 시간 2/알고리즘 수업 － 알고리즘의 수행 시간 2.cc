74ca86204655a628ff8ae934d2a3345e546c9070
HYE MIN PARK <phm6318@sookmyung.ac.kr>
2025-12-15 21:21:38 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std; 
+
+
+int main(){
+    int n, answer;     
+    cin >> n; 
+
+    /* Pseudo Code 
+    MenOfPassion(A[], n) {
+        sum <- 0;                         //대입(초기화) O(1)
+        for i <- 1 to n                   //반복 O(n)
+            sum <- sum + A[i]; # 코드1    //덧셈O(1), 배열 접근 O(1), 대입 O(1)
+        return sum;                       // 반환 O(1)
+    }
+    */
+    cout << n << "\n";
+    cout << 1 << "\n";
+    
+}
