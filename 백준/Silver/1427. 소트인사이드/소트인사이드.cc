ccdcbf60fca154f9cd40b1e1e3485ac5c7c98a3f
HYE MIN PARK <phm6318@sookmyung.ac.kr>
2026-01-04 22:59:19 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main(){
+    string str; 
+    cin >> str; 
+    sort(str.begin(), str.end());
+    reverse(str.begin(), str.end());
+    cout << str;
+}
