6b39db1e0deb513c75ebf092be9c8e14cf93a7be
HYE MIN PARK <phm6318@sookmyung.ac.kr>
2026-03-03 22:23:17 +0900

    [Silver III] Title: 두 수의 합, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,43 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 int main(){
-    int n, x, count=0; 
+    int n,x;
+    int cnt = 0; 
     cin >> n; 
-    vector<int> arr(n);
-    
-    for (int k=0; k<n; k++){
-        cin >> arr[k]; 
+    vector<int> nums (n,0);
+    for (int i=0; i<n; i++){
+        cin >> nums[i];
     }
+    cin >> x;
 
-    cin >> x; 
-    
-    sort(arr.begin(), arr.end());
-    int i=0, j=n-1; 
-    // 양끝의 원소들을 더한다
-    // 더한 값이 x보다 작으면 i인덱스 +1 
-    // 더한 값이 x보다 크면 j인덱스 -1 
-    // 더한 값이 x와 같으면 i인덱스 +1 && j인덱스 -1
-    // i 가 j보다 작아야하는 것이 반복문의 조건 
-
-    while (i < j) {
-        if (arr[i]+arr[j] == x) {
-            i += 1;
-            j -= 1;
-            count += 1; 
+    sort(nums.begin(), nums.end());
+    int head=0, tail=n-1;
+    while(head<tail) {
+        if (nums[head] + nums[tail] == x) {     
+            cnt ++; 
+            head++;
+            tail--;
         }
-        else if (arr[i]+arr[j] < x) {
-            i += 1;
+        else if (nums[head] + nums[tail] > x) {
+            tail--;
         }
-        else if (arr[i]+arr[j] > x) {
-            j -= 1;
+        else if (nums[head] + nums[tail] < x) {
+            head++;
         }
     }
+    cout << cnt;
     
-   
-    cout << count;
-    return 0; 
     
 }
