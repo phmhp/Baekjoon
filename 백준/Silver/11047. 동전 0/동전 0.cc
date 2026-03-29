0904e43aace09500b127f7f224f657cf3992c862
HYE MIN PARK <phm6318@sookmyung.ac.kr>
2026-03-29 21:12:28 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,29 +1,29 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std; 
 
-int main() {
+// 동전은 총 n종류 
+// 동전을 적절히 사용해서 가치의 합을 k로 만들 때, 필요한 동전 개수의 최솟값 
+int main(){
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(nullptr);
+    int n, k; 
+    int cnt = 0 ; 
+    cin >> n >> k;
 
-    vector<int> money;
-    int N,K,price;
-    int count=0; 
-    cin >> N >> K;
-    
-    
-    for (int j = 0 ; j<N; j++){
-        cin >> price;
-        money.push_back(price);    
+    vector<int> A(n); 
+    for (int i=0; i<n; i++){
+        int temp;
+        cin >> A[i]; //오름차순으로 입력 
     }
 
-    for (int i = N-1; i>=0; i--){
-         
-        if (money[i] <= K){
-            
-            count += K/money[i];
-            K -=(K/money[i]) * money[i];
-        } 
+    //출력: k원을 만드는데 필요한 동전 개수의 최솟값 
+    // -> 배열을 뒤에서부터 돌면서 k보다 작은 최대 coin이 몇개씩 들어갈 수 있는지 파악  
+    for (int j=n-1; j>=0; j--) {
+        if (A[j] > k) continue; 
+        cnt += k/A[j]; 
+        k-=A[j]*(k/A[j]);
+
+        if (k==0) break; 
     }
-    cout << count;
+    cout << cnt; 
 }
